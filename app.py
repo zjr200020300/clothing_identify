72a83a9b52ac4e77441e5a3dbd91b30990cbe0bd
zjr200020300 <50257909+zjr200020300@users.noreply.github.com>
2020-03-31 23:02:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,35 +6,39 @@ from tensorflow import keras
 import tensorflow as tf
 import os
 import time
-from flask import Flask
+import datetime as dt
+from flask import Flask, jsonify
 from cassandra.cluster import Cluster
+from cassandra.query import SimpleStatement
 import logging
 log = logging.getLogger()
 log.setLevel('INFO')
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter(
-    "%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
+handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
 log.addHandler(handler)
+
+global fr
 cluster = Cluster(contact_points=['127.0.0.1'], port=9042)
 session = cluster.connect()
 KEYSPACE = "mykeyspace"
 app = Flask(__name__)
+
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+# TensorFlow and tf.keras
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-
 def createKeySpace():
-    log.info("Creating keyspace...")
-    try:
-        session.execute("""
+   log.info("Creating keyspace...")
+   try:
+       session.execute("""
            CREATE KEYSPACE IF NOT EXISTS %s
            WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
            """ % KEYSPACE)
-        log.info("setting keyspace...")
-        session.set_keyspace(KEYSPACE)
-        log.info("creating table...")
-        session.execute("""
+       log.info("setting keyspace...")
+       session.set_keyspace(KEYSPACE)
+       log.info("creating table...")
+       session.execute("""
            CREATE TABLE mytable (
                time text,
                name text,
@@ -42,15 +46,13 @@ def createKeySpace():
                PRIMARY KEY (name)
            )
            """)
-    except Exception as e:
-        log.error("Unable to create keyspace")
-        log.error(e)
+   except Exception as e:
+       log.error("Unable to create keyspace")
+       log.error(e)
 
+createKeySpace();
 
-createKeySpace()
-
-
-def insertdata(fr, perdict):
+def insertdata(fr,perdict):
 
     try:
         n = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
@@ -60,6 +62,7 @@ def insertdata(fr, perdict):
             """ % (n, fr, perdict))
         log.info("%s, %s, %s" % (n, fr, perdict))
         log.info("Data stored!")
+        session.execute("""Select * from mytable;""")
     except Exception as e:
         log.error("Unable to insert data!")
         log.error(e)
@@ -77,14 +80,13 @@ def plot_image(i, predictions_array, img):
 
 
 @app.route('/file/<file>', methods=['GET'])
-def upload_file(file):
-    global fr
+def anas(file):
     fr = '/Users/a912158001/Desktop/' + file
-
     def anal(fr):
         model = keras.models.load_model(
             '/Users/a912158001/PycharmProjects/image_classification/my_model.h5')
         test_images = Image.open(fr)
+        #print(type(test_images))
         test_images = np.invert(test_images.convert('L'))
         test_images = test_images / 255.0
         probability_model = tf.keras.Sequential([model,
@@ -94,5 +96,7 @@ def upload_file(file):
         b = plot_image(0, predictions_single[0], test_images)
         return b
     perdict = anal(fr)
-    insertdata(fr, perdict)
+    insertdata(fr,perdict)
     return perdict
+
+#anas('pullover1.jpg')
